89e94d63a4a555abb062bf87b7ac6e53a38031ce
Katerine Linda Witkoski <ka.lwitkoski@gmail.com>
2022-06-09 12:31:48 -0300

    documenta as funções

--- R/subir_bq.R
+++ R/subir_bq.R
@@ -1,3 +1,12 @@
+#' Função para subir base no Goosle Big Query
+#'
+#' Essa função conecta com o google big query e escreve a
+#' data base com um nome específico
+#'
+#' @param da base.
+#' @param nome nome desejado.
+#'
+#' @export
 subir_bq <- function(da, nome) {
   da_subir <- da |>
     dplyr::mutate(dt_upload = Sys.Date())
--- R/utils-bq.R
+++ R/utils-bq.R
@@ -1,6 +1,11 @@
+#' Função para conectar com o Google Big Query
+#'
+#' Essa função conecta com o google big query
+#'
+#' @export
 bq_connect <- function() {
 
-  path_json <- system.file("bq.json", package = "obsCIEE")
+  path_json <- system.file("bq.json", package = "tjsp2inst")
   bigrquery::bq_auth(path = path_json)
 
   con <- bigrquery::dbConnect(
